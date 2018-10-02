8a504d5c73ba7a80e7d6535205e8f315f1b7ada4
Sergey Karnyushin <karnyushin.sergey@pdffiller.team>
2018-10-02 18:57:31 +0300

    SAL-3310 | class accesses checking added

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # PermissionTest
+
+This script checks if all your apex classes is accessible to the chosen permission set. It returns "Success"
+or error message.
+
+How it works:
+
+* Download file "permissionstest.php"
+* Make it executable "chmod u+x ./permissionstest.php"
+* Create file "setup.json" looks like this:
+```
+{
+  "file": "/Users/someUser/IdeaProjects/MyOrg1/src/permissionsets/DaDaDocs_Set.permissionset",
+  "path": "/Users/someUser/IdeaProjects/MyOrg1/src/classes"
+}
+```
+where "file" - path to permission set file and "path" - path to apex classes folder
+* run "./permissionstest.php"
--- permissionstest.php
+++ permissionstest.php
@@ -1,28 +1,110 @@
+#!/usr/bin/env php
 <?php
 
 $testInstance = new TestPermissionSet();
-$testInstance->test();
+try {
+    $testInstance->test();
+    echo "Success\n";
+} catch (Exception $e) {
+    echo "Error: {$e->getMessage()}\n";
+}
 
 class TestPermissionSet
 {
     private $exceptions = [
     ];
 
+    private $setup;
+
+    private $permissionSet;
+
+    /**
+     * @throws Exception
+     */
     public function test(): void
     {
-        $setup = $this->getSetup();
-        $permissionSet = new SimpleXMLElement($setup['file']);
+        $classNames = $this->getClassNames();
+        foreach ($classNames as $className) {
+            $this->checkClass($className, $this->getClassAccesses());
+        }
     }
 
+    /**
+     * @throws Exception
+     */
+    private function getClassNames(): array
+    {
+        $classNames = [];
+        $setup = $this->getSetup();
+        $allNames = scandir($setup['path']);
+        foreach ($allNames as $name) {
+            if (is_file("{$setup['path']}/$name") && (substr($name, -4) != '.xml')) {
+                $classNames[] = substr($name, 0, -4);
+            }
+        }
+        return $classNames;
+    }
+
+    /**
+     * @return array
+     * @throws Exception
+     */
+    private function getClassAccesses(): array
+    {
+        $classAccesses = [];
+        $permissionSet = $this->getPermissionSet();
+        foreach ($permissionSet['classAccesses'] as $classAccess) {
+            if ($classAccess['enabled'] == 'true') {
+                $classAccesses[] = $classAccess['apexClass'];
+            }
+        }
+        return $classAccesses;
+    }
+
+    /**
+     * @return array
+     * @throws Exception
+     */
+    private function getPermissionSet(): array
+    {
+        if (is_null($this->permissionSet)) {
+            $setup = $this->getSetup();
+            $permissionSet = new SimpleXMLElement(file_get_contents($setup['file']));
+            $this->permissionSet = json_decode(json_encode($permissionSet), true);
+        }
+        return $this->permissionSet;
+    }
+
+    /**
+     * @return array
+     * @throws Exception
+     */
     private function getSetup(): array
     {
-        return json_decode(file_get_contents('setup.json'), true);
+        if (is_null($this->setup)) {
+            if (!file_exists('setup.json')) {
+                throw new \Exception('Setup file not found!');
+            }
+            $this->setup = json_decode(file_get_contents('setup.json'), true);
+            if (!(isset($this->setup['file']) && isset($this->setup['path']))) {
+                throw new \Exception('Incorrect setup format!');
+            }
+        }
+        return $this->setup;
     }
 
-    private function checkFile(string $filename): void
+    /**
+     * @param string $className
+     * @param array $classAccesses
+     * @throws Exception
+     */
+    private function checkClass(string $className, array $classAccesses): void
     {
-        if (in_array($filename, $this->exceptions)) {
+        if (in_array($className, $this->exceptions)) {
             return;
+        }
+        if (!in_array($className, $classAccesses)) {
+            throw new Exception("$className is absent in permission set.");
         }
     }
 }
